1a8d8cefc50864c75717d976f26265d5b611cfa2
KakoytoChel1 <vanyarusin2402@gmail.com>
2026-02-26 23:31:09 +0200

    Data models added, Views modified

---
+++ FileSystem-Viewer/Models/DirectoryNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FileSystem_Viewer.Models
+{
+    public class DirectoryNode : FileSystemNode
+    {
+        public DirectoryNode(string name, string fullPath, long size, DateTime lastModified) : base(name, fullPath, size, lastModified) { }
+
+        public DirectoryNode(string name, string fullPath, long size, DateTime lastModified, IEnumerable<FileNode> fileNodes) : base(name, fullPath, size, lastModified)
+        {
+            _fileNodes = new ObservableCollection<FileNode>(fileNodes);
+        }
+
+        private ObservableCollection<FileNode>? _fileNodes = null;
+        public ObservableCollection<FileNode>? FileNodes
+        {
+            get { return _fileNodes; }
+            set { SetProperty(ref _fileNodes, value); }
+        }
+    }
+}
---
+++ FileSystem-Viewer/Models/FileSystemNode.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace FileSystem_Viewer.Models
+{
+    public abstract class FileSystemNode : ObservableObject
+    {
+        private string _name = null!;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private string _fullPath = null!;
+        public string FullPath
+        {
+            get { return _fullPath; }
+            set { SetProperty(ref _fullPath, value); }
+        }
+
+        private long _size;
+        public long Size
+        {
+            get { return _size; }
+            set { SetProperty(ref _size, value); }
+        }
+
+        private DateTime _lastModified;
+        public DateTime LastModified
+        {
+            get { return _lastModified; }
+            set { SetProperty(ref _lastModified, value); }
+        }
+        protected FileSystemNode(string name, string fullPath, long size, DateTime lastModified)
+        {
+            Name = name;
+            FullPath = fullPath;
+            Size = size;
+            LastModified = lastModified;
+        }
+    }
+}
